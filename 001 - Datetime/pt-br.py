865925dc96c0a03506ca2be4e8763d0ed5ea4a31
unknown <julianodesenv@gmail.com>
2021-10-13 10:22:32 -0300

    days pt-br

--- 001 - Datetime/pt-br.py
+++ 001 - Datetime/pt-br.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from locale import setlocale, LC_ALL
-from calendar import mdays
+from calendar import mdays, monthrange
 
 setlocale(LC_ALL, '')
 setlocale(LC_ALL, 'pt_BR.utf-8')
@@ -16,3 +16,15 @@ formatacao2 = dt.strftime('%d/%m/%Y %H:%M:%S')
 
 print(formatacao1, formatacao2)
 print(mes_atual, mdays[mes_atual])
+
+# Retorna uma tupla contendo o número do dia na semana (0-6)
+# e último dia de fevereiro de 2020
+print(monthrange(2020, 2))
+
+dia_semana, ultimo_dia = monthrange(2020, 2)
+print(ultimo_dia, dia_semana)  # Saída: 29 (último dia de fevereiro de 2020)
+
+ultimos_dias_de_meses_do_ano_atual = [
+    monthrange(datetime.now().year, mes)[1] for mes in range(1, 13)
+]
+print(ultimos_dias_de_meses_do_ano_atual)
