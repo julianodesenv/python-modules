d1d901faaa1a4563452110b7a1c646393515b42d
unknown <julianodesenv@gmail.com>
2021-10-15 10:32:31 -0300

    jupyter

---
+++ 012 - Jupyter/main.py
@@ -0,0 +1,9 @@
+"""
+Jupyter notebook é uma aplicação web que permite a execução de código em tempo real,
+visualização de dados e textos explicativos.
+Cientistas de dados podem criam, executar o código e explicar o processo enquanto executam
+e mostrar os dados.
+
+pip install jupyter
+pipenv install jupyter
+"""
